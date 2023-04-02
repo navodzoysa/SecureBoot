65600f35d93a73e93c10591e775924de776a9ad5
Navod Zoysa <navod.zoysa@gmail.com>
2023-04-02 18:54:28 +0530

    Add pm2 commands

--- ota-server/app.js
+++ ota-server/app.js
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../ui/public')));
+app.use(express.static(path.join(__dirname, '../ui/build')));
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
