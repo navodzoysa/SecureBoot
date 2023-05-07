9949039c1c9ef4818c8225868b88d3b407faf675
Navod Zoysa <navod.zoysa@gmail.com>
2023-05-08 00:29:27 +0530

    Update OTA logic with modularized code

--- device-firmware/esp32/src/HTTPSHandler.hpp
+++ device-firmware/esp32/src/HTTPSHandler.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <ArduinoJson.h>
+#include <FS.h>
+#include <HTTPClient.h>
+#include <LittleFS.h>
+#include <Update.h>
 #include <WiFiClientSecure.h>
 
 class HTTPSHandler {
@@ -23,25 +28,136 @@ public:
     return false;
   }
 
-  void checkNewFirmware(const char *firmwareVersion) {
-    this->client->printf("GET https://secureboot.online/api/firmware/latest/esp32?currentVersion=%s HTTP/1.0\n", firmwareVersion);
-    this->client->println("Host: secureboot.online");
-    this->client->println("Connection: close");
-    this->client->println();
-    while (this->client->connected()) {
-      String line = this->client->readStringUntil('\n');
-      if (line == "\r") {
-        Serial.println("headers received");
-        break;
+  String checkNewFirmware(const char *currentFirmwareVersion) {
+    HTTPClient httpClient;
+    char url[strlen("https://secureboot.online/api/firmware/latest/"
+                    "esp32?currentVersion=") +
+             strlen(currentFirmwareVersion) + strlen(" HTTP/1.0")];
+
+    strcpy(
+        url,
+        "https://secureboot.online/api/firmware/latest/esp32?currentVersion=");
+    strcat(url, currentFirmwareVersion);
+    strcat(url, " HTTP/1.0\n");
+    const char *latestVersion;
+
+    if (httpClient.begin(*this->client, url)) {
+      Serial.printf("[HTTPS] GET %s\n", url);
+      int httpCode = httpClient.GET();
+      if (httpCode > 0) {
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+        String payload = httpClient.getString();
+        StaticJsonDocument<512> doc;
+        DeserializationError error = deserializeJson(doc, payload);
+        if (error) {
+          Serial.print(F("deserializeJson() failed: "));
+          Serial.println(error.c_str());
+          return "";
+        }
+        latestVersion = doc["firmwareVersion"];
+        Serial.printf("firmware ver - %s\n", latestVersion);
       }
     }
-
-    while (this->client->available()) {
-      char c = this->client->read();
-      Serial.write(c);
-    }
+    httpClient.end();
     this->client->stop();
+    return String(latestVersion);
   }
+
+  void downloadFirmware(const char *url) {
+    HTTPClient httpClient;
+
+    if (httpClient.begin(*this->client, url)) {
+      Serial.printf("[HTTPS] GET %s\n", url);
+      int httpCode = httpClient.GET();
+      if (httpCode > 0) {
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+
+        if (!LittleFS.begin(true)) {
+          Serial.println("An Error has occurred while mounting LittleFS");
+          return;
+        }
+        File file = LittleFS.open("/firmware.bin", "w");
+        if (!file) {
+          Serial.println("Failed to open file for writing");
+          return;
+        }
+
+        int length = httpClient.getSize();
+        uint8_t buff[128] = {0};
+        auto stream = httpClient.getStreamPtr();
+
+        Serial.println("Writing firmware to flash:");
+        while (httpClient.connected() && (length > 0 || length == -1)) {
+          int c =
+              stream->readBytes(buff, std::min((size_t)length, sizeof(buff)));
+          Serial.printf("readBytes: %d\n", c);
+          Serial.printf("length: %d\n", length);
+          if (!c) {
+            Serial.println("Read timeout!");
+            ESP.restart();
+          }
+          size_t bytesWritten = file.write(buff, c);
+          if (bytesWritten == 0) {
+            Serial.println("could not write to the file");
+            file.close();
+            LittleFS.end();
+            return;
+          }
+          if (length > 0) {
+            length -= c;
+          }
+          delay(1);
+        }
+
+        file.close();
+        LittleFS.end();
+        delay(3000);
+      } else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n",
+                      httpClient.errorToString(httpCode).c_str());
+      }
+      httpClient.end();
+
+      if (!LittleFS.begin()) {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+      }
+
+      File file = LittleFS.open("/firmware.bin");
+
+      if (!file) {
+        Serial.println("Failed to open file for reading");
+        return;
+      }
+
+      Serial.println("Starting update..");
+
+      size_t fileSize = file.size();
+
+      if (!Update.begin(fileSize)) {
+        Serial.println("Cannot do the update");
+        return;
+      };
+
+      Update.writeStream(file);
+
+      if (Update.end()) {
+        Serial.println("Successful update");
+      } else {
+        Serial.println("Error Occurred: " + String(Update.getError()));
+        return;
+      }
+
+      file.close();
+
+      Serial.println("Reset in 4 seconds...");
+      delay(4000);
+      ESP.restart();
+    } else {
+      Serial.println("Unable to connect to OTA server");
+    }
+  }
+
   void postRequest();
   void putRequest();
 };
--- device-firmware/esp32/src/main.cpp
+++ device-firmware/esp32/src/main.cpp
@@ -3,7 +3,6 @@
 #include "ProvisioningCredentials.hpp"
 #include "WiFiHandler.h"
 #include <Arduino.h>
-#include <HttpsOTAUpdate.h>
 #include <Preferences.h>
 #include <WiFiClientSecure.h>
 #include <stdint.h>
@@ -14,34 +13,8 @@ static const char *password = "";
 static const char *preSharedKey =
     "530635c845c3db3aeeb4dbbc6c9567ef9ed31565374918af2cf1573e80e8cdfb";
 
-static const char *url =
-    "https://dl.dropboxusercontent.com/s/7y8nasjntpp0l6g/firmware.bin";
-
-static const char *server_certificate =
-    "-----BEGIN CERTIFICATE-----\n"
-    "MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n"
-    "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
-    "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n"
-    "QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n"
-    "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n"
-    "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n"
-    "9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n"
-    "CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n"
-    "nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n"
-    "43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n"
-    "T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n"
-    "gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n"
-    "BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n"
-    "TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n"
-    "DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n"
-    "hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n"
-    "06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n"
-    "PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n"
-    "YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n"
-    "CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n"
-    "-----END CERTIFICATE-----\n";
-
 const char *server = "secureboot.online";
+const char *url = "https://secureboot.online/api/firmware/download/6457a5ed32689715b47f923b";
 
 const char *test_root_ca =
     "-----BEGIN CERTIFICATE-----\n"
@@ -81,34 +54,6 @@ Preferences preferences;
 HTTPSHandler httpsHandlder(&client);
 ProvisioningCredentials provisioningCredentials(&preferences);
 
-static HttpsOTAStatus_t otastatus;
-
-void HttpEvent(HttpEvent_t *event) {
-  switch (event->event_id) {
-  case HTTP_EVENT_ERROR:
-    Serial.println("Http Event Error");
-    break;
-  case HTTP_EVENT_ON_CONNECTED:
-    Serial.println("Http Event On Connected");
-    break;
-  case HTTP_EVENT_HEADER_SENT:
-    Serial.println("Http Event Header Sent");
-    break;
-  case HTTP_EVENT_ON_HEADER:
-    Serial.printf("Http Event On Header, key=%s, value=%s\n", event->header_key,
-                  event->header_value);
-    break;
-  case HTTP_EVENT_ON_DATA:
-    break;
-  case HTTP_EVENT_ON_FINISH:
-    Serial.println("Http Event On Finish");
-    break;
-  case HTTP_EVENT_DISCONNECTED:
-    Serial.println("Http Event Disconnected");
-    break;
-  }
-}
-
 String savedPreSharedKey;
 bool isProvisioned;
 
@@ -116,6 +61,7 @@ void setup() {
   Serial.begin(115200);
   Serial.printf("Booted %s with ChipID - %" PRIu64 "\n", ESP.getChipModel(),
                 ESP.getEfuseMac());
+  Serial.printf("Firmware Version - %s", firmwareVersion);
 
   appInitialize();
 
@@ -128,9 +74,9 @@ void setup() {
 
   connectWifi(ssid, password);
   httpsHandlder.initializeHttpsClient(test_root_ca);
-  httpsHandlder.connectHttpsClient(server);
-  httpsHandlder.putRequest();
-  httpsHandlder.checkNewFirmware(firmwareVersion);
+  String latestVersion = httpsHandlder.checkNewFirmware(firmwareVersion);
+  Serial.printf("New firmware - %s\n", latestVersion);
+  // httpsHandlder.downloadFirmware(url);
 }
 
 void loop() {
